84b6af378c11550f3c8cc867961b559aafd31d06
ANGELVELAZ51 <angelgerald05@outlook.com>
2024-08-29 17:50:21 -0600

    Funcionamiento completo

---
+++ src/routes/api.docs.ts
@@ -0,0 +1,24 @@
+const swaggerJsDocs= require('swagger-jsdoc');
+
+const options ={
+    swaggerDefinition:{
+        info: {
+            version: "1.0.0",
+            title: "Sistema de ventas API",
+            description: "Documentacion API",
+            contact: {
+                name: "Angel Gerardo Velazquez",
+                url:"https://github.com"
+            },
+            serves:[{
+                url:"http://localhost:3000",
+                description:"Developer server"
+            }]
+        }
+    },
+    basePath:"/",
+    apis:["./src/routes/*.ts"]
+};
+
+const swaggerDocs = swaggerJsDocs(options);
+export default swaggerDocs;
